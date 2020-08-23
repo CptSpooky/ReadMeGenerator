11bbce2a45a9a06a8b7ee4ff77f286f92e56e613
CptSpooky <legacyofthedonut@gmail.com>
2020-08-23 19:09:22 -0400

    License options

--- index.js
+++ index.js
@@ -12,9 +12,63 @@ const questions = [
 
     {
         type: "input",
-        message: "Whats the project name?",
+        message: "What's the App name?",
         name: "title"
-
+    },
+    {
+        type: "input",
+        message: "Enter deployed App link.",
+        name: "deployed"
+    },
+    {
+        type: "input",
+        message: "Take a screenshot or gif of your App and enter the url.",
+        name: "appImg"
+    },
+    {
+        type: "input",
+        message: "What is your App about? Your process?",
+        name: "description"
+    },
+    {
+        type: "input",
+        message: "What is the installation process?",
+        name: "install"
+    },
+    {
+        type: "input",
+        message: "How do you use the App?",
+        name: "usage"
+    },
+    {
+        type: "input",
+        message: "What license are you using? Please input a number. \n 1. MIT \n 2. GPL \n 3. Apache \n 4. Other\n?" ,
+        name: "license"
+    },
+    {
+        type: "input",
+        message: "What are the stipulations if others what to contribute to your App?" ,
+        name: "contribute"
+    },
+    {
+        type: "input",
+        message: "What is your GitHub user name?" ,
+        name: "gitUser"
+    },
+    {
+        type: "input",
+        message: "What is your GitHub profile link?" ,
+        name: "gitLink"
+    },
+    {
+        type: "input",
+        message: "What is your Email?" ,
+        name: "email"
+    },
+    {
+        type: "input",
+        message: "How should people go about contacting you?" ,
+        name: "contactMe"
     }
 
 ];
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,5 +1,25 @@
 // function to generate markdown for README
 var generateMarkdown = function(data) {
+
+  // license markdown
+  const licenses = {
+    MIT: "[MIT](https://opensource.org/licenses/MIT)",
+    GPL: "[GPL](https://www.gnu.org/licenses/gpl-3.0.en.html)",
+    Apache: "[Apache](https://www.apache.org/licenses/LICENSE-2.0)",
+    Other: "Unspecified (user specific)."
+  }
+
+  // license switch
+  let license = null;
+  switch (data.license){
+    case "1": license = licenses.MIT; break;
+    case "2": license = licenses.GPL; break;
+    case "3": license = licenses.Apache; break;
+    default: 
+    case "4": license = licenses.Other; break;
+  }
+
+  // markdown
   return `# ${data.title}
 
   ## Table of Contents
@@ -12,21 +32,31 @@ var generateMarkdown = function(data) {
   - [Questions](#Questions)
 
   ## Description
-  test
+  [App Link](${data.deployed})
+  ![App Img](${data.appImg})
+
+  ${data.description}
 
   ## Installation
+  ${data.install}
 
   ## Usage
+  ${data.usage}
 
   ## License
+  ${license}
 
   ## Contributing
+  ${data.contribute}
 
   ## Tests
+  ${data.test}
 
   ## Questions
+  [GitHub: ${data.gitUser}](${data.gitLink})
+  [Email: ${data.email}](${data.email})
+  Contact me! ${data.contactMe}
 
-  
   `;
 }
 
