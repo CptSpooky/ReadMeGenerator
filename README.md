b82250efc0d1f31fa09ccababf2e105478f63eb9
CptSpooky <legacyofthedonut@gmail.com>
2020-08-23 18:00:02 -0400

    readme link test

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
-# honk
+# Honk
 
   ## Table of Contents
-  - [Description](#heading-1)
-  - [Installation](#heading-2)
-  - [Usage](#heading-3)
-  - [License](#heading-4)
-  - [Contributing](#heading-5)
-  - [Tests](#heading-6)
-  - [Questions](#heading-7)
+  - [Description](#Description)
+  - [Installation](#Installation)
+  - [Usage](#Usage)
+  - [License](#License)
+  - [Contributing](#Contributing)
+  - [Tests](#Tests)
+  - [Questions](#Questions)
 
   ## Description
   test
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -3,13 +3,13 @@ var generateMarkdown = function(data) {
   return `# ${data.title}
 
   ## Table of Contents
-  - [Description](#heading-1)
-  - [Installation](#heading-2)
-  - [Usage](#heading-3)
-  - [License](#heading-4)
-  - [Contributing](#heading-5)
-  - [Tests](#heading-6)
-  - [Questions](#heading-7)
+  - [Description](#Description)
+  - [Installation](#Installation)
+  - [Usage](#Usage)
+  - [License](#License)
+  - [Contributing](#Contributing)
+  - [Tests](#Tests)
+  - [Questions](#Questions)
 
   ## Description
   test
