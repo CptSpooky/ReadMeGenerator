c759c527b749ce7750fc2201b4f7e02c9fdbc467
CptSpooky <legacyofthedonut@gmail.com>
2020-08-23 16:17:59 -0400

    pulled in module

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
+const addModule = require("./utils/generateMarkdown.js");
+
+const markdown = addModule.generateMarkdown;
+const fileName = "README.md";
+
+
 // array of questions for user
 const questions = [
 
@@ -5,11 +14,18 @@ const questions = [
 
 // function to write README file
 function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) =>{ 
+        if (err) {
+            return console.log(err);
+        }
+
+        console.log("success!");
+    });
 }
 
 // function to initialize program
 function init() {
-
+    writeToFile();
 }
 
 // function call to initialize program
